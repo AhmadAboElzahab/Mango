e495d741806359bb26f0c9c7176fe794c8ffea99
Ahmad Abo Elzahab <server.vbs@gmail.com>
2025-05-06 12:40:18 +0400

    refactor: remove unused user token display from login route component

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -1,18 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import LoginContainer from '../../containers/Auth/Login';
-import { useAuthStore } from 'store/auth.store';
 
 export const Route = createFileRoute('/auth/login')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const user = useAuthStore((state) => state.user);
-
   return (
     <div>
-      {user && <p>{user.token}</p>}
       <LoginContainer />
     </div>
   );
